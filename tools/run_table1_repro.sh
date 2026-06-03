876fdfa9040188fd0deff72b5bb531440ed71522
maomaocun <maomaocun@users.noreply.github.com>
2026-06-03 19:18:42 +0800

    Sync Table 1 repro configs

--- tools/run_table1_repro.sh
+++ tools/run_table1_repro.sh
@@ -12,61 +12,63 @@ export NUMEXPR_NUM_THREADS="${NUMEXPR_NUM_THREADS:-1}"
 PYTHON="${PYTHON:-$ROOT/.venv/bin/python}"
 SELECT="${1:-all}"
 
+GPUS="${GPUS:-0,1,2,3,4,5,6,7}"
+
 run_eval() {
   echo
   echo "==> $*"
-  "$PYTHON" tools/ncfm_eval_tuner.py "$@"
+  "$PYTHON" tools/run_eval_repeats_parallel.py "$@"
 }
 
 run_cifar10() {
   run_eval --tag table1_c10_i1_vr10 \
     --config config/ipc1/cifar10.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/CIFAR-10/CIFAR10_ipc1.pt" \
-    --ipc 1 --val-repeat 10 --gpu 4,5,6,7 --nproc 4 --port 45001
+    --ipc 1 --val-repeat 10 --gpu "$GPUS" --port-base 45000
 
   run_eval --tag table1_c10_i10_vr10 \
     --config config/ipc10/cifar10.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/CIFAR-10/CIFAR10_ipc10.pt" \
-    --ipc 10 --val-repeat 10 --gpu 4,5,6,7 --nproc 4 --port 45010
+    --ipc 10 --val-repeat 10 --gpu "$GPUS" --port-base 45100
 
   run_eval --tag table1_c10_i50_vr10 \
     --config config/ipc50/cifar10.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/CIFAR-10/CIFAR10_ipc50.pt" \
-    --ipc 50 --val-repeat 10 --gpu 0,1,2,3,4,5,6,7 --nproc 8 --port 45050
+    --ipc 50 --val-repeat 10 --gpu "$GPUS" --port-base 45200
 }
 
 run_cifar100() {
   run_eval --tag table1_c100_i1_vr10 \
     --config config/ipc1/cifar100.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/CIFAR-100/CIFAR100_ipc1.pt" \
-    --ipc 1 --val-repeat 10 --gpu 4,5,6,7 --nproc 4 --port 45101
+    --ipc 1 --val-repeat 10 --gpu "$GPUS" --port-base 45300
 
   run_eval --tag table1_c100_i10_vr10 \
     --config config/ipc10/cifar100.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/CIFAR-100/CIFAR100_ipc10.pt" \
-    --ipc 10 --val-repeat 10 --gpu 0,1,2,3,4,5,6,7 --nproc 8 --port 45110
+    --ipc 10 --val-repeat 10 --gpu "$GPUS" --port-base 45400
 
   run_eval --tag table1_c100_i50_vr10 \
     --config config/ipc50/cifar100.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/CIFAR-100/CIFAR100_ipc50.pt" \
-    --ipc 50 --val-repeat 10 --gpu 0,1,2,3,4,5,6,7 --nproc 8 --port 45150
+    --ipc 50 --val-repeat 10 --gpu "$GPUS" --port-base 45500
 }
 
 run_tinyimagenet() {
   run_eval --tag table1_tiny_i1_vr10 \
     --config config/ipc1/tinyimagenet.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/Tiny ImageNet/TinyImageNet_ipc1.pt" \
-    --ipc 1 --val-repeat 10 --gpu 0,1 --nproc 2 --port 45201
+    --ipc 1 --val-repeat 10 --gpu "$GPUS" --port-base 45600
 
   run_eval --tag table1_tiny_i10_vr10 \
     --config config/ipc10/tinyimagenet.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/Tiny ImageNet/TinyImageNet_ipc10.pt" \
-    --ipc 10 --val-repeat 10 --gpu 2,3,4,5 --nproc 4 --port 45210
+    --ipc 10 --val-repeat 10 --gpu "$GPUS" --port-base 45700
 
   run_eval --tag table1_tiny_i50_vr10 \
     --config config/ipc50/tinyimagenet.yaml \
     --load-path "hf_data/NCFM_distillation_dataset/Tiny ImageNet/TinyImageNet_ipc50.pt" \
-    --ipc 50 --val-repeat 10 --gpu 6,7 --nproc 2 --port 45250
+    --ipc 50 --val-repeat 10 --gpu "$GPUS" --port-base 45800
 }
 
 case "$SELECT" in
